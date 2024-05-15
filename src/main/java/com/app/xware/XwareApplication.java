b5a24425b7b52b43c83fb1e12aebcfdab18dc769
Quangz <quang@kobizo.de>
2024-05-16 00:08:03 +0700

    first commit

---
+++ src/main/java/com/app/xware/XwareApplication.java
@@ -0,0 +1,13 @@
+package com.app.xware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XwareApplication.class, args);
+	}
+
+}
