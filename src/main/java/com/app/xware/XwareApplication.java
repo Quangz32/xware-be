3e2467fd2321f48f61c96d5c2caf113346184a21
Quangz <quang@kobizo.de>
2024-05-16 09:09:21 +0700

    app properties

--- src/main/java/com/app/xware/XwareApplication.java
+++ src/main/java/com/app/xware/XwareApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XwareApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Quang");
 		SpringApplication.run(XwareApplication.class, args);
 	}
 
